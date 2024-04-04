be752e7880bc3f15dd214817afae5e9394bb0212
Asma Afshar <161859558+AsmaAfshar@users.noreply.github.com>
2024-04-04 12:58:44 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() *6 + 1);
+console.log( "Welcome to number guessing game:");
+const number = await inquirer.prompt([
+ {
+name: "userGuessedNumber",
+type: "number",
+message: " Please guess a number between 1-6: ",
+ },
+]);
+
+if(number.userGuessedNumber === randomNumber){
+    console.log("Congratulations! you guess a correct number.") 
+}else {
+    console.log("Sorry! you guess a wrong number");
+};
